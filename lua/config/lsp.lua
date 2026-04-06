40239787c17181e5f2d9a82d6f67245a6b32939b
cwilliams0926 <coopwilliams03@hotmail.com>
2026-04-06 11:34:47 -0400

    Add primeagen sets and remaps

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -16,6 +16,12 @@ vim.lsp.config('lua_ls', {
   },
 })
 
+vim.api.nvim_create_autocmd('BufWritePre', {
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
+
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     local opts = { buffer = ev.buf }
