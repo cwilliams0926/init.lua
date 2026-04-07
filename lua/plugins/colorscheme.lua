6e40d490ec8335f6f37cd8afcce284fffbe52227
cwilliams0926 <coopwilliams03@hotmail.com>
2026-04-07 17:33:48 -0400

    Switch to rosepine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,22 @@
 return {
     {
-	"folke/tokyonight.nvim",
-	config = function() 
-	    vim.cmd.colorscheme "tokyonight"
-	end
+        "rose-pine/neovim",
+        name = "rose-pine",
+        priority = 1000,
+        config = function()
+            require("rose-pine").setup({
+                variant = "moon", -- auto, main, moon, dawn
+            })
+            vim.cmd.colorscheme("rose-pine")
+        end
     },
     {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-	    "nvim-tree/nvim-web-devicons",
-	},
-	opts = {
-	    theme = 'tokyonight'
-	}
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            theme = "rose-pine"
+        }
     },
 }
