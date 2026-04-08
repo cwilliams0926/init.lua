df030d13030e7d3a4101a445348c8c100996a2b2
cwilliams0926 <coopwilliams03@hotmail.com>
2026-04-08 16:10:19 -0400

    Add prettier and stuff

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -2,10 +2,21 @@ vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+
+-- html tab width fix
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "html",
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.expandtab = true
+    end
+})
 
 vim.opt.wrap = false
 
