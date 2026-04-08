fbb029d434b08c6a12e839287cffcb499513cc02
cwilliams0926 <coopwilliams03@hotmail.com>
2026-04-08 12:30:42 -0400

    Add virtual lines

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,36 +1,40 @@
 vim.lsp.config('emmet_language_server', {
-  init_options = {
-      preferences = {
-	["output.indent"] = "    ",  -- 4 spaces
-      }
+    init_options = {
+        preferences = {
+            ["output.indent"] = "    ", -- 4 spaces
+        }
     }
 })
 
 vim.lsp.config('lua_ls', {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' },
-      },
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' },
+            },
+        },
     },
-  },
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-  callback = function()
-    vim.lsp.buf.format()
-  end,
+    callback = function()
+        vim.lsp.buf.format()
+    end,
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-    -- etc.
-  end,
+    callback = function(ev)
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        -- etc.
+    end,
 })
 
-
+vim.diagnostic.config({
+    virtual_lines = {
+        current_line = true
+    },
+})
