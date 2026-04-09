91ab97db8d9d4bd24cbead2863372adc57d9c3a6
cwilliams0926 <coopwilliams03@hotmail.com>
2026-04-09 12:39:42 -0400

    Add oneliners to oneliners.lua

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -1,14 +1,29 @@
 return {
-    { -- show CSS colors
-        'brenoprata10/nvim-highlight-colors',
-        config = function()
-            require('nvim-highlight-colors').setup({})
-        end
-    },
-    { -- git plugin
-        'tpope/vim-fugitive',
-    },
-    { -- lspconfig
-        "neovim/nvim-lspconfig",
-    }
+	{ -- show CSS colors
+		"brenoprata10/nvim-highlight-colors",
+		config = function()
+			require("nvim-highlight-colors").setup({})
+		end,
+	},
+	{ -- git plugin
+		"tpope/vim-fugitive",
+	},
+	{ -- lspconfig
+		"neovim/nvim-lspconfig",
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
 }
